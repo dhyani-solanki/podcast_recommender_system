8134df3f90c66379d7b4cb05227532c01d2d9c8d
Dhyani Solanki <dhyani.solanki1724@gmail.com>
2025-07-05 14:56:25 +0530

    app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from emotion import detect_face_emotion, record_audio, extract_voice_features
 
 app = Flask(__name__)
 
-CLIENT_ID = "c8b7e54cca1548be8971646d6e983746"
-CLIENT_SECRET = "71427c9e1c1e43edad7a63f510a218a7"
+CLIENT_ID = "******"
+CLIENT_SECRET = "*****"
 
 @app.route('/')
 def index():
